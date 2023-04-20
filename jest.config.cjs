48624ba628b57f588c0d3e82fd42098a3f8069b7
Johannes Pahle <joe@jo-dev.de>
2023-04-20 23:08:22 +0200

    🧪 improve jest tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,14 @@
 module.exports = {
-  transform: { "^.+\\.ts?$": "ts-jest" },
-  testEnvironment: "node",
-  testRegex: "/tests/.*\\.(test|spec)?\\.(ts|tsx)$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
 };
